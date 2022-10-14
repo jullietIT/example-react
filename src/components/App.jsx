ce2e1b5c700359d3738c543d0869df799a571e8d
Juliya Khanenko <jullietnm@gmail.com>
2022-10-15 00:15:13 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import { RecipeList } from './RecipeList/RecipeList';
+import recipes from '../data/recipe.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <RecipeList recipes={recipes} />
+    </>
   );
 };
